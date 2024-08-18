89efdbaae2f5cfc0336eddd94bc7b4784e16ba8a
nextixt <nextixt@gmail.com>
2024-08-18 19:04:50 +0300

    Add files via upload

---
+++ neuralnetwork.py
@@ -0,0 +1,19 @@
+import numpy as np
+import random
+def sigmoid(x):
+    return 1 / (1 + 2.71828182845904 ** -x)
+input1 = np.array([1,0,1,0])
+input2 = np.array([0,1,0,1])
+
+
+weight1 = round(random.uniform(0, 1), 1)
+weight2 = round(random.uniform(0, 1), 1)
+if weight1 != weight2:
+    for i in range(1000):
+        output = sigmoid(input1 * weight1 + input2 * weight2)
+print(output)
+
+
+
+
+
