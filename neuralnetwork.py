c45f7bb803ee227e0e90f4425bcee7f417d7ef5b
Nextixt <nextixt@gmail.com>
2024-08-20 12:37:23 +0300

    neuralnetwork.py

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -5,8 +5,7 @@ def sigmoid(x):
 input1 = np.array([1,0,1,0])
 input2 = np.array([0,1,0,1])
 
-for i in range(1000):
-    weight1 = round(random.uniform(0, 1), 1)
-    weight2 = round(random.uniform(0, 1), 1)
+weight1 = round(random.uniform(0, 1), 1)
+weight2 = round(random.uniform(0, 1), 1)
 output = sigmoid(input1 * weight1 + input2 * weight2)
 print(output)
