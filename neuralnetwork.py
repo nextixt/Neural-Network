eb335ad8136a31ad55a1b6460efd23cbe3019ba6
Nextixt <nextixt@gmail.com>
2025-08-04 16:45:14 +0300

    Small updates
    Added "Loss" sign and removed ineffective code.

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -14,8 +14,7 @@ for i in range(200):
 
     w = w - learning_rate * loss * X
     b = b - learning_rate * loss
-print(loss)
-
-y_pred = sigmoid(w * X + b)
+print(f"Loss: {loss}")
 print(f"Prediction: {y_pred}")
 print(f"Current w: {w}, Current b: {b}")
+
