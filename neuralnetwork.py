1827234e082dcd221044e6a3126536fdf55d553f
Nextixt <nextixt@gmail.com>
2025-08-06 23:25:29 +0300

    Big Updates, 3rd version
    A very big update contains new train dataset with matrix, matrix operations. Also has been added Numpy framework and improved sigmoid function

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,20 +1,26 @@
 import random
+import numpy as np
 def sigmoid(sum):
-    return 1 / (1 + 2.7183 ** -sum)
-X = -1
-y = 1
+    return 1 / (1 + np.exp(-sum))
+X = np.array([
+    [0,1,1,0],
+    [1,1,0,1],
+    [0,1,0,0],
+    [1,1,0,0]
+    ])
+
+y = np.array([0,1,1,0])
 b = 0.1
-w = random.uniform(-1,1)
+w = np.random.uniform(-1, 1, size=X.shape[1])
 learning_rate = 0.01 
 
 for i in range(200):
-    y_pred = sigmoid(w * X + b)
+    y_pred = sigmoid(w @ X + b)
 
     loss = (y_pred - y) ** 2
 
-    w = w - learning_rate * loss * X
+    w = w - learning_rate * loss @ X
     b = b - learning_rate * loss
-print(f"Loss: {loss}")
+print(f"Loss: {sum(loss)}")
 print(f"Prediction: {y_pred}")
 print(f"Current w: {w}, Current b: {b}")
-
