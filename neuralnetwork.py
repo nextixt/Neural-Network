87fbc6b538edda13460edac6c561ce1f8a81ed57
Nextixt <nextixt@gmail.com>
2025-08-06 23:40:02 +0300

    Version 3.1 fixed bugs
    I've fixed some very problematic bugs with loss and prediction. Also comments have been added and gradient system.

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -11,16 +11,20 @@ X = np.array([
 
 y = np.array([0,1,1,0])
 b = 0.1
+# generating matrix of weights
 w = np.random.uniform(-1, 1, size=X.shape[1])
 learning_rate = 0.01 
 
 for i in range(200):
     y_pred = sigmoid(w @ X + b)
+    # counting loss
+    loss = np.mean((y_pred - y) ** 2)
+    # counting gradients
+    grad = X.T @ (loss * y_pred * (1 - y_pred))
+    #updating weights and bias
+    w -= learning_rate * grad
+    b -= learning_rate * np.sum(loss * y_pred * (1 - y_pred))
 
-    loss = (y_pred - y) ** 2
-
-    w = w - learning_rate * loss @ X
-    b = b - learning_rate * loss
-print(f"Loss: {sum(loss)}")
+print(f"Loss: {loss}")
 print(f"Prediction: {y_pred}")
 print(f"Current w: {w}, Current b: {b}")
